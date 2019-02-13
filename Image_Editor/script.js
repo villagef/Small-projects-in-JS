fa68735946dfaf6a0c1e13a6cf3aecc115e535dc
villagef <filwyd123@gmail.com>
2019-02-13 13:14:49 +0100

    download image and add image to canvas add

--- Image_Editor/script.js
+++ Image_Editor/script.js
@@ -85,3 +85,72 @@ revertBtn.addEventListener("click", e => {
     this.revert();
   });
 });
+
+// Upload File
+uploadFile.addEventListener("change", () => {
+    // Get File
+    const file = document.getElementById("upload-file").files[0];
+    // Init FileReader API
+    const reader = new FileReader();
+  
+    // Check for file
+    if (file) {
+      // Set file name
+      fileName = file.name;
+      // Read data as URL
+      reader.readAsDataURL(file);
+    }
+  
+    // Add image to canvas
+    reader.addEventListener(
+      "load",
+      () => {
+        // Create image
+        img = new Image();
+        // Set image src
+        img.src = reader.result;
+        // On image load add to canvas
+        img.onload = function() {
+          canvas.width = img.width;
+          canvas.height = img.height;
+          ctx.drawImage(img, 0, 0, img.width, img.height);
+          canvas.removeAttribute("data-caman-id");
+        };
+      },
+      false
+    );
+  });
+  
+  // Download Event
+  downloadBtn.addEventListener("click", () => {
+    // Get ext
+    const fileExtension = fileName.slice(-4);
+  
+    // Init new filename
+    let newFilename;
+  
+    // Check image type
+    if (fileExtension === ".jpg" || fileExtension === ".png") {
+      // new filename
+      newFilename = fileName.substring(0, fileName.length - 4) + "-edited.jpg";
+    }
+  
+    // Call download
+    download(canvas, newFilename);
+  });
+  
+  // Download
+  function download(canvas, filename) {
+    // Init event
+    let e;
+    // Create link
+    const link = document.createElement("a");
+  
+    // Set props
+    link.download = filename;
+    link.href = canvas.toDataURL("image/jpeg", 0.8);
+    // New mouse event
+    e = new MouseEvent("click");
+    // Dispatch event
+    link.dispatchEvent(e);
+  }
